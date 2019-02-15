e02df6cd14d38530572c834a684241c0108e3d11
Alec Hall <alechall1990@yahoo.com>
2019-02-14 18:58:58 -0600

    stopping point, trying to connect DBs

--- main.py
+++ main.py
@@ -12,22 +12,24 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-    username = db.Cloumn(db.String(120), unique=True)
+    username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
-    tasks = db.relationship('Task', backref='owner')
+    blogs = db.relationship('Blog', backref='owner')
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, blogs):
         self.username = username
         self.password = password
+        self.blogs = blogs
 
 @app.route('/')
 def index():
@@ -68,13 +70,18 @@ def new_post():
     
     return render_template('newpost.html', title='New Entry')
 
-@app.route('/signup')
+@app.route('/login', methods=['POST','GET'])
+def login():
+    return render_template('login.html')
 
-@app.route('/login')
 
-@app.route('/index')
+#@app.route('/signup')
 
-@app.route('/logout')
 
-if  __name__ == "__main__":
+
+#@app.route('/index')
+
+#@app.route('/logout')
+
+if __name__ == "__main__":
     app.run()
